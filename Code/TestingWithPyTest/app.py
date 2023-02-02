85b7827b3b465b012ee144bc7eebe4904ab9ce84
Kevin Cunningham <github@kevincunningham.co.uk>
2023-02-02 08:58:34 +0000

    removes files

--- Code/TestingWithPyTest/app.py
+++
@@ -1,6 +0,0 @@
-from project import create_app
-
-
-# Call the application factory function to construct a Flask application
-# instance using the development configuration
-app = create_app('flask.cfg')
--- Code/TestingWithPyTest/instance/README.md
+++
@@ -1,5 +0,0 @@
-# Instance Folder
-
-## Purpose
-
-The `instance` folder is used to store run-time data, including log files and database files (SQLite).
