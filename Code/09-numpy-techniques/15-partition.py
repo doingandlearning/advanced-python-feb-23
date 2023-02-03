42eedf2c4390a95550f1bdc35c718b3ead7827b2
Kevin Cunningham <github@kevincunningham.co.uk>
2023-02-03 10:00:22 +0000

    shows correct partition example

--- Code/09-numpy-techniques/15-partition.py
+++ Code/09-numpy-techniques/15-partition.py
@@ -1,13 +1,20 @@
 import numpy as np
 
-a = np.random.randint(0, 101, 12)
+# a = np.random.randint(0, 101, 12)
 
-print('Unpartitioned 1D array', a)
-print('Partitioned at index 2', np.partition(a, 2))
-print('Partitioned at index 4', np.partition(a, 4))
-print('Sorted array          ', np.sort(a))
+# print('Unpartitioned 1D array', a)
+# print('Partitioned at index 2', np.partition(a, 2))
+# print('Partitioned at index 4', np.partition(a, 4))
+# print('Sorted array          ', np.sort(a))
 
-b = np.random.randint(0, 101, 49).reshape((7,7))
-print('\nUnsorted 2D array\n', b)
-print('\nSorted across cols\n', np.sort(b, axis=1))
-print('\nSorted down rows  \n', np.sort(b, axis=0))
+# # b = np.random.randint(0, 101, 49).reshape((7,7))
+# # print('\nUnsorted 2D array\n', b)
+# # print('\nSorted across cols\n', np.sort(b, axis=1))
+# # print('\nSorted down rows  \n', np.sort(b, axis=0))
+
+a = np.arange(0,20)
+np.random.shuffle(a)
+
+print(a)
+print(np.partition(a, 2))
+print(np.partition(a, 10))
